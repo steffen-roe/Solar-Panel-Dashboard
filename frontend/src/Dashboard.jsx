89804c1930e05e1d8e0ea4181cbcaffa389a74b2
steffen-roe <steffen.roesner@proton.me>
2024-12-12 15:49:31 +0100

    Add new React build

---
+++ frontend/src/Dashboard.jsx
@@ -0,0 +1,13 @@
+import "./Dashboard.css"
+
+function Dashboard() {
+  
+    return (
+      <div>
+        <div className="title">Solar Panel Dashboard</div>
+      </div>
+    )
+  }
+  
+  export default Dashboard
+  
