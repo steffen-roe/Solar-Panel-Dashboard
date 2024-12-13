c2aaf387f338b2f798b1ca49bd525c505adb4daf
steffen-roe <steffen.roesner@proton.me>
2024-12-13 15:58:05 +0100

    Add dropdown menu to toolbar

--- frontend/src/components/ChartContainer/ChartContainer.jsx
+++ frontend/src/components/ChartContainer/ChartContainer.jsx
@@ -1,16 +1,23 @@
+import React, { useState } from 'react';
 import styles from "./ChartContainer.module.css"
 import InfoToolbar from "../InfoToolbar/InfoToolbar"
 import ChartRenderer from "../ChartRenderer/ChartRenderer"
 
 function ChartContainer() {
-  
-    return (
+  const [range, setRange] = useState("day"); // Default range
+
+  // Handle range change
+  const handleRangeChange = (newRange) => {
+    setRange(newRange);
+  };
+
+  return (
       <div className={styles.chartContainer}>
-        <InfoToolbar/>
-        <ChartRenderer/>
+        <InfoToolbar range={range} onRangeChange={handleRangeChange}/>
+        <ChartRenderer range={range}/>
       </div>
-    )
-  }
+  )
+}
   
-  export default ChartContainer
+export default ChartContainer
   
--- frontend/src/components/ChartRenderer/ChartRenderer.jsx
+++ frontend/src/components/ChartRenderer/ChartRenderer.jsx
@@ -1,13 +1,14 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Chart from 'chart.js/auto';
 import styles from "./ChartRenderer.module.css";
 import useApi from '../../api/useApi';
 
-function ChartRenderer() {
+function ChartRenderer({ range }) {
   const canvasRef = useRef(null);
   const chartRef = useRef(null);
   
-  const { data, error } = useApi("day"); 
+  const { data, error } = useApi(range); 
+  const [type, setType] = useState("line");
 
   useEffect(() => {
     if (data && data.length > 0) {
@@ -23,97 +24,165 @@ function ChartRenderer() {
       let powerData = [];
       let energyData = [];
 
-      labels = data.map(({ time }) => time);
-      powerData = data.map(({ power }) => power);
-      energyData = data.map(({ energy }) => energy);
+      if (range === "day" || range.length === 8) {
+        labels = data.map(({ time }) => time); 
+        powerData = data.map(({ power }) => power); 
+        energyData = data.map(({ energy }) => energy); 
+        setType("line");
+      } else if (range === "7" || range === "30") {
+        labels = data.map(({ date }) => date); 
+        energyData = data.map(({ max_energy }) => max_energy); 
+        setType("bar");
+      }
 
-      const type = "line"; 
-
-      chartRef.current = new Chart(canvas, {
-        type: type,
-        data: {
-          labels,  // Time or Date labels
-          datasets: [
-            {
-              label: 'Power Output (W)',
-              data: powerData,
-              borderColor: '#FF6F00',
-              backgroundColor: 'rgba(255, 111, 0, 0.2)',
-              tension: 0.4,
-              fill: false,
-              yAxisID: 'y',
-              pointRadius: 0,
-              borderWidth: 4,
+      if (type === "line") {
+        chartRef.current = new Chart(canvas, {
+          type: 'line', // Choose chart type based on range
+          data: {
+            labels,
+            datasets: [
+              {
+                label: 'Power Output (W)',
+                data: powerData,
+                borderColor: '#FF6F00',
+                backgroundColor: 'rgba(255, 111, 0, 0.2)',
+                tension: 0.4, 
+                fill: false,
+                yAxisID: 'y', 
+                pointRadius: 0, 
+                borderWidth: 4,
+              },
+              {
+                label: 'Energy Generated (kWh)',
+                data: energyData, 
+                borderColor: '#007BFF',
+                backgroundColor: 'rgba(0, 123, 255, 0.2)', 
+                tension: 0.4, 
+                fill: true, 
+                yAxisID: 'y1',
+                pointRadius: 0, 
+                borderWidth: 4,
+              },
+            ],
+          },
+          options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            plugins: {
+              legend: {
+                labels: {
+                  color: '#ffffff',
+                  font: {
+                    size: 14,
+                  },
+                  boxHeight: '0',
+                },
+                display: true,
+                position: 'bottom',
+                align: 'center',
+              },
             },
-            {
-              label: 'Energy Generated (kWh)',
-              data: energyData,
-              borderColor: '#007BFF',
-              backgroundColor: 'rgba(0, 123, 255, 0.2)',
-              tension: 0.4,
-              fill: true,
-              yAxisID: 'y1',
-              pointRadius: 0,
-              borderWidth: 4,
-            },
-          ],
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-            legend: {
-              labels: {
-                color: '#ffffff',
-                font: {
-                  size: 14,
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'Time of Day', 
+                },
+                grid: {
+                  color: 'rgba(255, 255, 255, 0)',
+                },
+                ticks: {
+                  maxTicksLimit: 10, 
                 },
               },
-              display: true,
-              position: 'bottom',
-              align: 'center',
+              y: {
+                title: {
+                  display: true,
+                  text: 'Power (W)',
+                },
+                grid: {
+                  color: 'rgba(255, 255, 255, 0.1)',
+                },
+                beginAtZero: true,
+                position: 'left', 
+              },
+              y1: {
+                title: {
+                  display: true,
+                  text: 'Energy (kWh)',
+                },
+                grid: {
+                  drawOnChartArea: false, 
+                },
+                position: 'right', 
+                beginAtZero: true,
+              },
             },
           },
-          scales: {
-            x: {
-              title: {
+        });
+      } else if (type === "bar") {
+        chartRef.current = new Chart(canvas, {
+          type: 'bar', // Choose chart type based on range
+          data: {
+            labels,
+            datasets: [
+              {
+                label: 'Energy Generated (kWh)',
+                data: energyData, 
+                borderColor: '#007BFF',
+                backgroundColor: 'rgba(0, 123, 255, 0.7)', 
+                tension: 0,
+                fill: true, 
+                pointRadius: 0,
+              },
+            ],
+          },
+          options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            plugins: {
+              legend: {
+                labels: {
+                  color: '#ffffff',
+                  font: {
+                    size: 14,
+                  },
+                },
                 display: true,
-                text: 'Time of Day',
-              },
-              grid: {
-                color: 'rgba(255, 255, 255, 0)',
-              },
-              ticks: {
-                maxTicksLimit: 10,
+                position: 'bottom',
+                align: 'center',
               },
             },
-            y: {
-              title: {
-                display: true,
-                text: 'Power (W)',
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'Date', 
+                },
+                grid: {
+                  color: 'rgba(255, 255, 255, 0)',
+                },
+                ticks: {
+                  maxTicksLimit: 7, 
+                },
               },
-              grid: {
-                color: 'rgba(255, 255, 255, 0.1)',
+              y: {
+                title: {
+                  display: true,
+                  text: 'Energy (kWh)',
+                },
+                grid: {
+                  color: 'rgba(255, 255, 255, 0.1)',
+                },
+                position: 'left',
+                beginAtZero: true,
               },
-              beginAtZero: true,
-              position: 'left',
-            },
-            y1: {
-              title: {
-                display: true,
-                text: 'Energy (kWh)',
-              },
-              grid: {
-                drawOnChartArea: false,
-              },
-              position: 'right',
-              beginAtZero: true,
             },
           },
-        },
-      });
+        });
+      }
     }
-  }, [data]); // Re-run when data changes
+  }, [data, range]); // Re-run when data changes
 
   return (
     <div>
