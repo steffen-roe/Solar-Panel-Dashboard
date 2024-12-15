d3ee546c1236e8aa681fa5ad860255233be435d8
steffen-roe <steffen.roesner@proton.me>
2024-12-15 14:50:30 +0100

    Put API key in env file

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'https://1hebxmo6bj.execute-api.eu-central-1.amazonaws.com'
-}); 
+  baseURL: `${import.meta.env.VITE_API}`,
+});
 
-export default apiClient; 
+export default apiClient;
